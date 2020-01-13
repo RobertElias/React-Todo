1d71f85da9b12a920f551320e2be9c29bde460c2
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-13 13:23:52 -0800

    Completed App.js with constructor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './components/TodoComponents/TodoList'
+import TodoForm from './components/TodoComponents/TodoForm'
 
-class App extends React.Component {
+
+const todo = [
+  {
+    name: 'Finish ToDo App',
+    id: 123,
+    completed: false
+
+  }
+ ]
+class App extends Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state ={
+      todoList: todo,
+      nextTodo: '',
+      show: true
+    }
+  }
+  toggleItem = id => {
+    // find the item we clicked on
+    // toggle the completed field
+    // update state with the new list data
+    const newTodoList = this.state.todoList.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          purchased: !item.purchased
+        };
+      } else {
+        return item;
+      }
+    });
+    // update groceryList
+    this.setState({
+      todoList: newTodoList
+    });
+  };
+
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className="App">
+          <div className="header">
         <h2>Welcome to your Todo App!</h2>
+        {/* TodoForm component */}
       </div>
+      {/* TodoList component */}
+      </div>
+     
     );
   }
 }
